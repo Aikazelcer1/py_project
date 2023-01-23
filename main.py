02eba32607a384aa87053cfeb299084460e81147
Aikazelcer1 <asya146@mail.ru>
2023-01-23 20:56:23 +0300

    Коммит

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
+from funcs.ry import ad
+from funcs.ry import add_n
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+ad(a, b)
+add_n(a, b)
 
 
-# Press the green button in the gutter to run the script.
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
